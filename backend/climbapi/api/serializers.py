74a674dbb98b67880de1522ba4e21576b7a5f767
Tudor Muntianu <tmuntia@yahoo.com>
2019-08-06 17:35:33 -0700

    misc changes

--- backend/climbapi/api/serializers.py
+++ backend/climbapi/api/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 from .models import Route, Profile, Comment
 
-class CommentSerializer(serializers.Serializer):
-    poster = serializers.SerializerMethodField()
-
-    def get_poster(self, obj):
-        return obj.poster.user.username
-    
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['poster', 'text', 'rating']
+        fields = '__all__'
+    
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        self.comments  = serializers.PrimaryKeyRelatedField(many=True, queryset=Comment.objects.filter(route=self.fields['route']))
+        super(RouteSerializer, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = Route
+        fields = '__all__'
